9c2e4bed62b2d1f4481f168d2fc1f29cf7f9d6a3
Laura Hoffman <lhoffmanwg@gmail.com>
2018-11-28 15:07:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 function dwarfRollCall(dwarves) {
-  var i = 0;
-  var stringList = '';
-  for(i = 0; i < dwarves.length; i++) {
-    
+  var stringList = "";
+  for(var i = 0; i < dwarves.length; i++) {
     stringList = stringList + (i+1) + ". " + dwarves[i] + " ";
   }
   return stringList;
 }
 
 function summonCaptainPlanet(planeteerCalls){
-  var i;
   var callsArray = [];
-  for(i = 0; i < planeteerCalls.length; i++){
+  for(var i = 0; i < planeteerCalls.length; i++){
       callsArray.push(planeteerCalls[i].toUpperCase() + "!");
  }
  return callsArray;
 }
 
 function longPlaneteerCalls(words) {
-  var i;
-  for(i=0; i < words.length; i++){
+  for(var i=0; i < words.length; i++){
      if(words[i].length > 4) {
        return true;
     }
@@ -29,10 +25,8 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
   var cheese = ["cheddar", "gouda", "camembert"];
-  var i;
-  for(i=0; i < foods.length; i++) {
-    var j;
-    for(j=0; j < cheese.length; j++) {
+  for(var i=0; i < foods.length; i++) {
+    for(var j=0; j < cheese.length; j++) {
       if(foods[i] === cheese[j]){
         return cheese[j];
       }
